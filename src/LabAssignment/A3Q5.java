2944cd3178e17f4ae1ba64409f4f9b84d8db7e2c
Abhinab Choudhury <132006996+abhinab-choudhury@users.noreply.github.com>
2023-12-06 03:41:32 +0530

    Update A3Q5.java

--- src/LabAssignment/A3Q5.java
+++ src/LabAssignment/A3Q5.java
@@ -6,10 +6,10 @@ public class A3Q5 {
 	public static void main(String[] args) {
 		Scanner obj = new Scanner(System.in);
 		
-		System.out.print("Input the year: ");
+		System.out.print("Input tzhe year: ");
 		int y = obj.nextInt();
 		
-		if((y % 400 == 0 || (y % 100 != 0)) && (y % 4 == 0)) {
+		if((y % 400 == 0) || ((y % 100 != 0) && (y % 4 == 0)) {
 			System.out.println( y + " is a leap year: true");
 		} else {
 			System.out.println( y + " is a leap year: false");
