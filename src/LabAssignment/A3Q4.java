91fd74174d9b7bf927cf29e2ba1a78fcf3343154
Abhinab Choudhury <132006996+abhinab-choudhury@users.noreply.github.com>
2023-12-08 22:28:51 +0530

    Update A3Q4.java

--- src/LabAssignment/A3Q4.java
+++ src/LabAssignment/A3Q4.java
@@ -8,7 +8,8 @@ public class A3Q4 {
 		int random_num = (int)(1 + (9 * Math.random()));
 		System.out.print("Enter user number: ");
 		int user_guess = obj.nextInt();
-	
+	        System.out.println("Computers choise : " + random_num);
+		
 		if(user_guess == random_num) {
 			System.out.println("You got it right");
 		} else if(Math.abs(random_num - user_guess) == 1) {
